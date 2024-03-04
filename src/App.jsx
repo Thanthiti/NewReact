7da157ee370366f264bcf12b97130884d7ae806c
Thanthiti <plam1234palm@gmail.com>
2024-03-04 09:31:08 +0700

    Tesst

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from './assets/Hello'
+import Form from './assets/Form'
+
+
+function App() {
+  
+  return (
+    
+      <div>
+       <Hello/>
+       <Form/>
+    </div>
+  )
+}
+
+export default App
